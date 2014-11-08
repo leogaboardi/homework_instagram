ffeaf603588b1018327c6ae7ac3fbc88ac65052f
Leonardo Gaboardi <leogaboardi@gmail.com>
2014-11-07 23:18:29 -0600

    HW6 v1
    Finished the CRUV part of the HW

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,49 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+    @id = params[:id]
+    @caption =  Photo.find_by({ :id => @id }).caption
+    @source =  Photo.find_by({ :id => @id }).source
 
+  end
+
+  def new_form
+
+  end
+
+  def edit_form
+    @id = params[:id]
+    @caption =  Photo.find_by({ :id => @id }).caption
+    @source =  Photo.find_by({ :id => @id }).source
+  end
+
+  def create_row
+    x = Photo.new
+    x.caption = params[:the_caption]
+    x.source = params[:the_source]
+    x.save
+
+    @caption = x.caption
+    @source = x.source
+
+  end
+
+  def update_row
+    @id = params[:id]
+    x = Photo.find_by({ :id => @id })
+    x.caption =  params[:the_caption]
+    x.source =  params[:the_source]
+    x.save
+
+    @caption = x.caption
+    @source = x.source
+    render("create_row")
+  end
+
+  def destroy
+    @id = params[:id]
+    x =  Photo.find_by({ :id => @id })
+    x.destroy
+  end
 end
